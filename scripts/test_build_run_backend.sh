690aacec2c2d79248f50bfab1a0116630ec75dbb
nkgithub01 <32886053+nkgithub01@users.noreply.github.com>
2026-05-07 21:44:46 -0500

    Delete scripts/test_build_run_backend.sh

--- scripts/test_build_run_backend.sh
+++
@@ -1,5 +0,0 @@
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-KERNEL_DIR=/workspace/kernels
-BACKEND=$1
-KERNEL=attention.hlo
-${SCRIPT_DIR}/docker.sh --compile -- ${SCRIPT_DIR}/build_backend.sh $BACKEND
